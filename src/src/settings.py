f8665b8449078eee4c19e903f0cf6468950d9e9b
sbiju <sekharanbijumon@gmail.com>
2016-03-04 14:57:34 +0530

    My Django Boys app, first commit

--- src/src/settings.py
+++ src/src/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'src.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "static_root")
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "media_root")
+STATICFILES_DIRS = [
+     os.path.join(BASE_DIR, "static"),
+    #'/var/www/static/',
+]
